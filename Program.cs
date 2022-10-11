d5704c6fbaa018f0554214cb06459a343bead6ac
johnmaster <juanpuhl404@gmail.com>
2022-10-10 21:47:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tp6_5_Cual_es_mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Ingrese por favor el numero 1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese por favor el numero 2: ");
+            num2=Convert.ToInt32(Console.ReadLine());
+
+            if(num1 > num2){
+                Console.WriteLine("El numero: " + num1 + " Es Mayor.");
+            }else {
+                Console.WriteLine("El numero: " + num2 + " Es Mayor.");
+            }
+        }
+    }
+}
